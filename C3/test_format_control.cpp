3eeef85491313df985eaf8e17635b3aff91a6506
PiFtch <PiFtch@126.com>
2018-04-14 22:11:02 +0800

    modify format_control

--- C3/test_format_control.cpp
+++ C3/test_format_control.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 int main() {
+    cout << left << right;
     cout << setw(8) << 222 << setw(8) << 33 << 33 << endl;
-    cout << setprecision(3) << 4.555 << 3.2576 << endl;
+    cout << setprecision(4) << 4.555 << 3.2576 << endl;
+    cout << showpoint << 2345.6547e7 << 2.3454e12;
     return 0;
 }
