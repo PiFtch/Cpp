e65be88341e6d9c5045cffd603a1240855bda6e9
PiFtch <PiFtch@126.com>
2018-04-14 21:57:37 +0800

    add test_format_control.cpp

---
+++ C3/test_format_control.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << setw(8) << 222 << setw(8) << 33 << 33 << endl;
+    cout << setprecision(3) << 4.555 << 3.2576 << endl;
+    return 0;
+}
